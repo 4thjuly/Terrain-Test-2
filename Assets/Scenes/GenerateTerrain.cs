e052dcd561aab0acbd774666dc2dc1c52da66f53
Ian Ellison-Taylor <ianet@microsoft.com>
2018-09-03 18:05:22 -0700

    water

--- Assets/Scenes/GenerateTerrain.cs
+++ Assets/Scenes/GenerateTerrain.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class GenerateTerrain : MonoBehaviour {
 
-    static int QUADS_PER_SIDE = 10;
+    static int QUADS_PER_SIDE = 50;
     static float HEIGHT_SCALE = 0.25f;
     static float DETAIL_SCALE = 0.5f;
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         Mesh mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
@@ -23,20 +24,16 @@ public class GenerateTerrain : MonoBehaviour {
         float uvFactor = 1.0f / QUADS_PER_SIDE;
         float scale = 1.0f / QUADS_PER_SIDE;
 
-        for (float y = 0.0f; y < QUADS_PER_SIDE + 1; y++)
-        {
-            for (float x = 0.0f; x < QUADS_PER_SIDE + 1; x++)
-            {
+        for (float y = 0.0f; y < QUADS_PER_SIDE + 1; y++) {
+            for (float x = 0.0f; x < QUADS_PER_SIDE + 1; x++) {
                 vertices[index] = new Vector3(x * scale - 1.0f / 2.0f, 0.0f, y * scale - 1.0f / 2.0f);
                 index++;
             }
         }
 
         index = 0;
-        for (int y = 0; y < QUADS_PER_SIDE; y++)
-        {
-            for (int x = 0; x < QUADS_PER_SIDE; x++)
-            {
+        for (int y = 0; y < QUADS_PER_SIDE; y++) {
+            for (int x = 0; x < QUADS_PER_SIDE; x++) {
                 triangles[index] = (y * (QUADS_PER_SIDE + 1)) + x;
                 triangles[index + 1] = ((y + 1) * (QUADS_PER_SIDE + 1)) + x;
                 triangles[index + 2] = (y * (QUADS_PER_SIDE + 1)) + x + 1;
@@ -49,32 +46,37 @@ public class GenerateTerrain : MonoBehaviour {
         }
 
         // Terrain adjust
-        for (int i = 0; i < vertices.Length; i++)
-        {
+        for (int i = 0; i < vertices.Length; i++)  {
             vertices[i].y = Mathf.PerlinNoise((vertices[i].x + this.transform.position.x) / DETAIL_SCALE, (vertices[i].z + this.transform.position.z) / DETAIL_SCALE) * HEIGHT_SCALE;
         }
 
         // Separate for flat look and color
         Vector3[] newVertices = new Vector3[triangles.Length];
         Color32[] colors = new Color32[triangles.Length];
-        for (int i = 0; i < triangles.Length; i++)
-        {
+        Color32 currentColor = new Color(0.0f, Random.Range(0.25f, 0.75f), 0.0f, 1.0f);
+        Color32 waterColor = new Color(0.0f, 0.0f, 1.0f, 1.0f);
+        for (int i = 0; i < triangles.Length; i++) {
             newVertices[i] = vertices[triangles[i]];
             triangles[i] = i;
+            colors[i] = currentColor; 
+            if ((i + 1) % 6 == 0) {
+                if (newVertices[i].y < 0.075f) { currentColor = waterColor; }
+                else { currentColor = new Color(0.0f, Random.Range(0.25f, 0.75f), 0.0f, 1.0f); }
+            }
         }
 
-        // Color
-        for (int i = 0; i < triangles.Length; )
-        {
-            // Color32 color = new Color(Random.Range(0.0f, 1.0f),  Random.Range(0.0f, 1.0f),  Random.Range(0.0f, 1.0f),  1.0f);
-            Color32 color = new Color(0.0f, Random.Range(0.25f, 0.75f), 0.0f, 1.0f);
-            colors[i++] = color;
-            colors[i++] = color;
-            colors[i++] = color;
-            colors[i++] = color;
-            colors[i++] = color;
-            colors[i++] = color;
-        }
+        //// Color
+        //for (int i = 0; i < triangles.Length; )
+        //{
+        //    // Color32 color = new Color(Random.Range(0.0f, 1.0f),  Random.Range(0.0f, 1.0f),  Random.Range(0.0f, 1.0f),  1.0f);
+        //    Color32 color = new Color(0.0f, Random.Range(0.25f, 0.75f), 0.0f, 1.0f);
+        //    colors[i++] = color;
+        //    colors[i++] = color;
+        //    colors[i++] = color;
+        //    colors[i++] = color;
+        //    colors[i++] = color;
+        //    colors[i++] = color;
+        //}
 
         mesh.vertices = newVertices;
         mesh.triangles = triangles;
@@ -88,20 +90,18 @@ public class GenerateTerrain : MonoBehaviour {
 
     void Update()
     {
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
-        Vector3[] vertices = mesh.vertices;
+    //    Mesh mesh = GetComponent<MeshFilter>().mesh;
+    //    Vector3[] vertices = mesh.vertices;
 
-        this.offsetX += MOVE_SPEED * Time.deltaTime;
+    //    this.offsetX += MOVE_SPEED * Time.deltaTime;
 
-        // this.transform.Translate(MOVE_SPEED * Time.deltaTime, 0f, 0f);
+    //    for (int i = 0; i < vertices.Length; i++)
+    //    {
+    //        vertices[i].y = Mathf.PerlinNoise((vertices[i].x + this.transform.position.x + this.offsetX) / DETAIL_SCALE, (vertices[i].z + this.transform.position.z) / DETAIL_SCALE) * HEIGHT_SCALE;
+    //    }
 
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            vertices[i].y = Mathf.PerlinNoise((vertices[i].x + this.transform.position.x + this.offsetX) / DETAIL_SCALE, (vertices[i].z + this.transform.position.z) / DETAIL_SCALE) * HEIGHT_SCALE;
-        }
-
-        mesh.vertices = vertices;
-        mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
+    //    mesh.vertices = vertices;
+    //    mesh.RecalculateNormals();
+    //    mesh.RecalculateBounds();
     }
 }
